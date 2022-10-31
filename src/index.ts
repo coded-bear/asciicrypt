5c3ece30a1985fe4981f5eabf4b931fa693de62a
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2022-10-31 19:29:26 +0100

    added encrypt function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+const alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+
+function letterPosition(char: string, letters: string) {
+    return letters.indexOf(char.toLowerCase());
+}
+
+export function createSecret() {}
+
+export function encrypt(char: string, secret: string) {
+    const cipher: string[] = [];
+
+    char.split('').forEach(element => {
+        const positionInAlphabet = letterPosition(element, alphabet);
+
+        cipher.push(secret[positionInAlphabet]);
+    });
+
+    return cipher.join('');
+}
+
+export function decrypt(cipher: string, secret: string) {}
