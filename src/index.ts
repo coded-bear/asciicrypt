51faac13a77cd9b34b9ffd6a85b401a7c3f67a7c
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2022-11-01 13:05:54 +0100

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-const alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 function letterPosition(char: string, letters: string): number {
     return letters.indexOf(char.toLowerCase());
 }
 
-function helper(char: string, char2: string, char3: string) {
+export function createSecret() {}
+
+export function encrypt(char: string, secret: string): string {
     const arr: string[] = [];
 
     char.split('').forEach((element: string) => {
-        const position = letterPosition(element, char2);
-
-        arr.push(char3[position]);
+        arr.push(secret[letterPosition(element, alphabet)]);
     });
 
     return arr.join('');
 }
 
-export function createSecret() {}
-
-export function encrypt(char: string, secret: string): string {
-    return helper(char, alphabet, secret);
-}
-
 export function decrypt(char: string, secret: string): string {
-    return helper(char, secret, alphabet);
+    const arr: string[] = [];
+
+    char.split('').forEach((element: string) => {
+        arr.push(alphabet[letterPosition(element, secret)]);
+    });
+
+    return arr.join('');
 }
