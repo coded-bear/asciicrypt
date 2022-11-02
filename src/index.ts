02f9641d37f7991aed02a1c27bd2d09279ea8852
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2022-11-02 18:06:37 +0100

    added eslint and tsconfig release

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ function fyShuffle(arr: string[]) {
     let i = arr.length;
 
     while (--i > 0) {
-        let randIndex = Math.floor(Math.random() * (i + 1));
+        const randIndex = Math.floor(Math.random() * (i + 1));
         [arr[randIndex], arr[i]] = [arr[i], arr[randIndex]];
     }
 
